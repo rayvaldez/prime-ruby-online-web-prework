b2b036dfbf4aa22bd3f2671646424c8c1d14c27a
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-10 23:51:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(numbers)
+  start = 2
+  if numbers > 1
+    range = (start..numbers-1).to_a
+    range.none? do |num_to_test|
+      numbers % num_to_test == 0
+    end
+  else
+    false
+  end
+end
